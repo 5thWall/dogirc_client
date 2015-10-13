bea4f0a539ca3d03444bfc2d76f94e7eb7043676
Andy <5thWall@gmail.com>
2015-10-12 18:31:43 -0700

    Send over notice messages.
    Refactor message handling to be more concise.
    
    Closes #3

--- lib/dogirc_client/privmsg_handler.ex
+++ lib/dogirc_client/privmsg_handler.ex
@@ -3,14 +3,9 @@ defmodule DogircClient.PrivmsgHandler do
 
   alias Dogirc.Command
 
-  def handle_event(%Command{type: :privmsg, message: msg, from: usr}, state) do
-    message = %{message: msg, nick: usr.nick, kind: "privmsg"}
-    DogircClient.Endpoint.broadcast!("irc:freenode", "message", message)
-    {:ok, state}
-  end
-
-  def handle_event(%Command{type: :action, message: msg, from: usr}, state) do
-    message = %{message: msg, nick: usr.nick, kind: "action"}
+  def handle_event(%Command{type: type, message: msg, from: usr}, state)
+  when type in [:privmsg, :action, :notice] do
+    message = %{message: msg, nick: usr.nick, kind: to_string(type)}
     DogircClient.Endpoint.broadcast!("irc:freenode", "message", message)
     {:ok, state}
   end
